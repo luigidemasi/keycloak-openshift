8e5454415050ac1406733a2f03f7fa751e89a4fb
clintea <clint.eastwool@gmail.com>
2015-10-13 16:06:12 +0200

    changed

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ fi
 export POSTGRES_PORT_5432_TCP_ADDR
 export POSTGRES_PORT_5432_TCP_PORT
 
-echo "Parameter: $TEST_PARAM" > /opt/jboss/keycloak/start_sh_test_parameter
+# echo "Parameter: $TEST_PARAM" > /opt/jboss/keycloak/start_sh_test_parameter
 
 # use this for export
 # exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/opt/jboss/keycloak/export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
