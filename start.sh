67792357fe4a6981622ce0e6da916ab9c6b9cab1
clintea <clint.eastwool@gmail.com>
2015-10-15 15:26:28 +0200

    import demo realm at start

--- start.sh
+++ start.sh
@@ -11,20 +11,20 @@ export POSTGRES_PORT_5432_TCP_PORT
 # echo "Parameter: $TEST_PARAM" > /opt/jboss/keycloak/start_sh_test_parameter
 
 # use this for export
-exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
--Dkeycloak.migration.action=export \
--Dkeycloak.migration.provider=singleFile \
--Dkeycloak.migration.file=/opt/jboss/keycloak/export.json \
--Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+#exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
+#-Dkeycloak.migration.action=export \
+#-Dkeycloak.migration.provider=singleFile \
+#-Dkeycloak.migration.file=/opt/jboss/keycloak/export.json \
+#-Dkeycloak.migration.strategy=OVERWRITE_EXISTING
 
 # use this for import the demo realm
 # exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4
-#exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
-#-Dkeycloak.migration.action=import \
-#-Dkeycloak.migration.provider=singleFile \
-#-Dkeycloak.migration.file=/opt/jboss/keycloak/import.json \
-#-Dkeycloak.migration.realmName=demo \
-#-Dkeycloak.migration.strategy=IGNORE_EXISTING
+exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
+-Dkeycloak.migration.action=import \
+-Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.file=/opt/jboss/keycloak/import.json \
+-Dkeycloak.migration.realmName=demo \
+-Dkeycloak.migration.strategy=IGNORE_EXISTING
 
 # configure SSL using self signed certificate and jboss-cli.sh
 # exec /opt/jboss/keycloak/bin/jboss-cli.sh --connect --file=configure-security.cli
