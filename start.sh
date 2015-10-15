cdde6f132aff7eb3e20d298ca3e1b8537211b912
clintea <clint.eastwool@gmail.com>
2015-10-15 13:46:47 +0200

    export activated

--- start.sh
+++ start.sh
@@ -11,10 +11,14 @@ export POSTGRES_PORT_5432_TCP_PORT
 # echo "Parameter: $TEST_PARAM" > /opt/jboss/keycloak/start_sh_test_parameter
 
 # use this for export
-# exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/opt/jboss/keycloak/export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
+-Dkeycloak.migration.action=export \
+-Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.file=/opt/jboss/keycloak/export.json \
+-Dkeycloak.migration.strategy=OVERWRITE_EXISTING
 
 # use this for import the demo realm
-exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4
+# exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4
 #exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.node.name=$HOSTNAME -b $HOSTNAME -u 230.0.0.4 \
 #-Dkeycloak.migration.action=import \
 #-Dkeycloak.migration.provider=singleFile \
